a290c8e290391941cbe8e04cc515df1ada7ed899
Dangyi Liu <dangyi.liu@tusimple.ai>
2018-12-05 20:40:27 -0800

    Add installation script

---
+++ sparsehash-config.cmake
@@ -0,0 +1,13 @@
+if(TARGET sparsehash)
+  return()
+endif()
+
+get_filename_component(sparsehash_INCLUDE_DIR "${CMAKE_CURRENT_LIST_FILE}" REALPATH)
+get_filename_component(sparsehash_INCLUDE_DIR "${sparsehash_INCLUDE_DIR}" DIRECTORY)
+get_filename_component(sparsehash_INCLUDE_DIR "${sparsehash_INCLUDE_DIR}" DIRECTORY)
+get_filename_component(sparsehash_INCLUDE_DIR "${sparsehash_INCLUDE_DIR}" DIRECTORY)
+get_filename_component(sparsehash_INCLUDE_DIR "${sparsehash_INCLUDE_DIR}" DIRECTORY)
+
+add_library(sparsehash INTERFACE IMPORTED)
+set_target_properties(sparsehash PROPERTIES
+	INTERFACE_INCLUDE_DIRECTORIES ${sparsehash_INCLUDE_DIR})
